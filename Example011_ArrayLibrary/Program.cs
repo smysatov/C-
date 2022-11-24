731ba7c3b63f9c1eefd4a4fd1d210b573e656e2f
Sergey <smysatov@gmail.com>
2022-11-24 11:51:37 +0300

    Added example011 method FillArray & PrintArray

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,31 @@
+﻿// создаем метод FillArray для заполнения массива
+
+void FillArray(int[] collection)
+{
+    //длина нового массива
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+       //заполняем массив случайными числами
+       collection[index] = new Random().Next(1, 10);
+       index++;
+    }
+}
+
+//создаем метод PrintArray для вывода на печать массива
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
