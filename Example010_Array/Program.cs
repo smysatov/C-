a71ad1e51a8e802735f42d9b0fa70024127b61b0
Sergey <smysatov@gmail.com>
2022-11-24 11:20:03 +0300

    Added example010_array

---
+++ Example010_Array/Program.cs
@@ -0,0 +1,19 @@
+﻿// Нахождение индекса элемента массива со значением find
+
+int [] array = {1,20,51,2,45,6,20,9};
+
+int n = array.Length;
+int find = 20;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
